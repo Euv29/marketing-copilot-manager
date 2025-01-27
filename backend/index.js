e48e86b85f41496eb54f06bae61a2e363a1c7b66
Venâncio Wapinda <euvenancio1@gmail.com>
2025-01-27 20:08:01 +0100

    Configuração para servir o frontend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const { authenticate, authorize } = require('./middleware/auth');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
