bcf904fc387a8e992e9b831c8b87793297f3c47d
Venâncio Wapinda <euvenancio1@gmail.com>
2025-01-28 18:13:15 +0100

    Configurar base URL da API dinamicamente

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-// const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-const baseURL = process.env.REACT_APP_API_URL || 'https://mysterious-beach-84770-b25cca31cb4f.herokuapp.com/api';
+const apiBaseURL = process.env.NODE_ENV === 'production'
+  ? 'https://mysterious-beach-84770-b25cca31cb4f.herokuapp.com/api'
+  : 'http://localhost:5000/api';
 
 const api = axios.create({
-  baseURL,
+  baseURL: apiBaseURL,
 });
 
 console.log('API baseURL:', api.defaults.baseURL);
