582084e9064d65e400109a07903d2391c6173ed0
Venâncio Wapinda <euvenancio1@gmail.com>
2025-01-28 12:24:15 +0100

    Add logs to check database connection

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,19 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  client.query('SELECT NOW()', (err, result) => {
+    release();
+    if (err) {
+      return console.error('Error executing query', err.stack);
+    }
+    console.log('Database connected:', result.rows);
+  });
+});
+
 const facebook = new Facebook({
   appId: process.env.FACEBOOK_APP_ID,
   secret: process.env.FACEBOOK_APP_SECRET,
