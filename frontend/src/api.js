2a216e941a2f6ffd7fedaa2e6bde8e241cb52c8e
Venâncio Wapinda <euvenancio1@gmail.com>
2025-01-28 13:42:28 +0100

    Ensure API URL is correct and registration/login requests are properly configured

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || 'https://mysterious-beach-84770-b25cca31cb4f.herokuapp.com/api',
 });
 
 console.log('API baseURL:', api.defaults.baseURL);
